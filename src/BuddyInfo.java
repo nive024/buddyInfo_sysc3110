13a98c9140df232a29a4c7bb054e714006d5e2da
Nivetha <nivethasivasaravanan@cmail.carleton.ca>
2022-09-19 13:38:57 -0400

    Added main function

---
+++ src/BuddyInfo.java
@@ -0,0 +1,32 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BuddyInfo() {
+        this("Bob", "123 Parkway Lane", "6131234567");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Nivetha", "4 the parkway", "6134135364");
+        System.out.println("Hello " + buddy.getName());
+    }
+}
