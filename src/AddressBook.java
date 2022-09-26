52f8291188004e35209c336d36ffa3e4f38a04e2
Nivetha <nivethasivasaravanan@cmail.carleton.ca>
2022-09-25 21:03:53 -0400

    Added Address Book

---
+++ src/AddressBook.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> addressBook;
+
+    public AddressBook () {
+        addressBook = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo b) {
+        addressBook.add(b);
+    }
+
+    public void removeBuddy(BuddyInfo b) {
+        addressBook.remove(b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Nivetha", "4 the parkway", "6134135364");
+        AddressBook addressBook1 = new AddressBook();
+        addressBook1.addBuddy(buddy);
+        addressBook1.removeBuddy(buddy);
+    }
+}
